9cd1bbb6581bdbbac755beab7b49d306ce45e76d
studentN <noreply@puppet.com>
2022-09-18 15:11:04 -0400

    adds api, product, and operation

--- main.tf
+++ main.tf
@@ -31,3 +31,46 @@ resource "azurerm_api_management" "apim" {
   publisher_email = "ericjaystevens+demogod@gmail.com"
   sku_name = "Developer_1"
 }
+
+resource "azurerm_api_manamgent_product" "product" {
+  product_id            = "crud"
+  api_management_name   = azurerm_api_management.apim.name
+  resource_group_name   = azurerm_api_management.apim.resource_group_name
+  display_name          = "crud"
+  subscription_required = false
+  approval_required     = true
+  published             = true
+}
+
+resource "azurerm_api_management_api" "api" {
+  api_management_name = azurerm_api_management.apim.name 
+  name                = "reskeyva"
+  resource_group_name   = azurerm_api_management.apim.resource_group_name
+  revision            = "1"
+  display_name        = "reskeyva backend"
+  path                = "reskeyva"
+  protocols           = ["https"]
+  
+}
+
+resource "azurerm_api_management_product_api" "protudctToApi" {
+  api_name            = azurerm_api_management.api.name
+  product_id          = azurerm_api_management.product.product_id
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name   = azurerm_api_management.apim.resource_group_name
+}
+
+resource "azurerm_api_managment_operation" "op" {
+  operation_id        = "set-value"
+  api_name            = azurerm_api_management.api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name   = azurerm_api_management.apim.resource_group_name
+  display_name        = "Set a value"
+  method              = "POST"
+  url_template        = "/reskeyva/{key}"
+  description         = "set a key value"
+
+  response {
+    status_code = 200
+  } 
+}
