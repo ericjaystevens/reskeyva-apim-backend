d754ddd6f7025c30eb8990a3bfdc2d647c6ecc7e
studentN <noreply@puppet.com>
2022-09-18 20:08:21 -0400

    add storage account and table

--- main.tf
+++ main.tf
@@ -93,3 +93,16 @@ resource "azurerm_api_management_api_operation_policy" "example" {
   xml_content = data.template_file.op_policy.rendered
 
 }
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "sareskeyva"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_table" "table" {
+  name                 = "reskeyva"
+  storage_account_name = azurerm_storage_account.sa.name
+}
